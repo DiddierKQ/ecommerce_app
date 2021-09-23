7800e82c3e881a46d1291be1b0bba22b5f658f65
DiddierKantun <Diddier.Kantun@theksquaregroup.com>
2021-09-23 13:38:29 -0500

    Login screen created

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(  
+      backgroundColor: const Color(0xFFd2c4a8),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF6f5015),
+        title: const Text(
+          "Coffee shop",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Container(        
+          margin: const EdgeInsets.all(32.0),
+          padding: const EdgeInsets.all(16.0),
+          height: 820,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                    padding: const EdgeInsets.all(40),
+                    child: Image.asset(
+                      "assets/icons/coffee-cup.png",
+                      width: 200,
+                    ),
+                    ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(bottom: 28),
+                      child: const Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(
+                          left: 8, right: 8, top: 16, bottom: 16),
+                      child: const TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(0xFF6f5015), width: 2.0),
+                          ),
+                          border: OutlineInputBorder(),
+                          label: Text(
+                            "Email address",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      child: const TextField(
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(0xFF6f5015), width: 2.0),
+                          ),
+                          border: OutlineInputBorder(),
+                          label: Text(
+                            "Password",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: const Color(0xFF6f5015),
+                          minimumSize: const Size(double.infinity, 60),
+                        ),
+                        onPressed: () {},
+                        child: const Text('Log in',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 24)),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        child: const Text(
+                          "Create a new account",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF6f5015),
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 28.0),
+                      alignment: Alignment.center,
+                      child: Row(
+                        //crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [                        
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                                padding: const EdgeInsets.all(20.0),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xff39579A),
+                                ),
+                                child: const Icon(
+                                  FontAwesomeIcons.facebookF,
+                                  color: Colors.white,
+                                ),
+                            ), //
+                          ),
+                          const SizedBox(width: 16.0,),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xffDF4A32),
+                              ),
+                              child: const Icon(
+                                FontAwesomeIcons.googlePlusG,
+                                color: Colors.white,
+                              ),
+                            ), //
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 36),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Developed by Diddier Kantun",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0xFF6f5015),
+                  ),
+                ),
+              ),              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
